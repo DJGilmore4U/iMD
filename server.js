af7ac1f7a3ed2966ea554a3452142e1736cdd68a
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-05-24 20:12:12 -0700

    trying to fix post on signin

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(passport.session()); // persistent login sessions
 
 
  //For Handlebars
-app.set('views', './app/views');
+app.set('views', path.join(__dirname, 'app/views'));
 app.engine('hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
